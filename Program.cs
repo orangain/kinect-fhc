20ee4e43aa81011db9b301a5757f1341af38b48f
orangain <orangain@gmail.com>
2013-02-16 17:03:37 +0900

    Kinectのサンプルを使って音声認識できるようにした。FHCとの連携はまだ

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
 
 namespace Kinect_FHC
 {
@@ -10,6 +14,99 @@ namespace Kinect_FHC
     {
         static void Main(string[] args)
         {
+            KinectSensor sensor = null;
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != sensor)
+            {
+                try
+                {
+                    // Start the sensor!
+                    sensor.Start();
+                }
+                catch (IOException)
+                {
+                    // Some other application is streaming from the same Kinect sensor
+                    sensor = null;
+                }
+            }
+
+            if (null == sensor)
+            {
+                Console.WriteLine("Kinect not found");
+                return;
+            }
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if (null == ri)
+            {
+                Console.WriteLine("Recognizer not found");
+                return;
+            }
+
+            // Create a SpeechRecognitionEngine object for the default recognizer in the en-US locale.
+            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(ri.Id))
+            {
+
+                // Create a grammar for finding services in different cities.
+                Choices services = new Choices(new string[] { "restaurants", "hotels", "gas stations" });
+                Choices cities = new Choices(new string[] { "Seattle", "Boston", "Dallas" });
+
+                GrammarBuilder findServices = new GrammarBuilder("Find");
+                findServices.Append(services);
+                findServices.Append("near");
+                findServices.Append(cities);
+
+                // Create a Grammar object from the GrammarBuilder and load it to the recognizer.
+                Grammar servicesGrammar = new Grammar(findServices);
+                recognizer.LoadGrammarAsync(servicesGrammar);
+
+                // Add a handler for the speech recognized event.
+                recognizer.SpeechRecognized +=
+                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                // Configure the input to the speech recognizer.
+                recognizer.SetInputToDefaultAudioDevice();
+
+                // Start asynchronous, continuous speech recognition.
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                // Keep the console window open.
+                while (true)
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            foreach (RecognizerInfo recognizer in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && "ja-JP".Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+
+            return null;
+        }
+
+        // Handle the SpeechRecognized event.
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine("Recognized text: " + e.Result.Text);
         }
     }
 }
