146f97c9fcc660101cb324a2e422f03d35c82556
orangain <orangain@gmail.com>
2013-03-02 15:04:22 +0900

    ビルド後にbin\Debug, bin\Release をzipにパッケージできるようにした。

---
+++ package.ps1
@@ -0,0 +1,20 @@
+﻿param(
+	[string]$destFile,
+	[string]$srcDir
+)
+
+[Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+
+function zip([string]$destFile, [string]$srcDir)
+{
+	$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+	$includeBaseDir = $false
+	[System.IO.Compression.ZipFile]::CreateFromDirectory($srcDir, $destFile, $compressionLevel, $includeBaseDir)
+}
+
+if (Test-Path $destFile)
+{
+	rm $destFile
+}
+
+zip "$destFile" "$srcDir"
